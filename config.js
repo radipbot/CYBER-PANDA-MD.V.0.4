fb46ee62bd708542e14dd2b2de9a578bb0224aea
radipbot <156444704+radipbot@users.noreply.github.com>
2024-01-27 19:17:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,15 +21,15 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+94773283124", "+94773283124"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94773283124", "+94773283124"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94773283124"];
+global.OwnerName = "GOST";
+global.BotName = "GOST TM";
+global.packname = "🦋 GOST TM 🦋";                             //Do not change.
+global.author = "GOST";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/Eo6QInzfzpqIERhnyDvHuE";
 
 
 //
